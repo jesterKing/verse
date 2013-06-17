8b7677f719106fcde9c150e9993081af057362f5
Nathan Letwory <nathan@letworyinteractive.com>
2013-06-17 18:15:18 +0300

    Add integral typedefs needed for Windows.

--- include/verse.h
+++ include/verse.h
@@ -44,7 +44,27 @@ extern "C" {
 
 #define VERSE_H
 
+#if defined(_WIN32) && defined(_MSC_VER) && _MSC_VER <= 1500
+
+/* The __intXX are built-in types of the visual compiler! So we don't
+ * need to include anything else here. */
+
+
+typedef signed __int8 int8_t;
+typedef signed __int16 int16_t;
+typedef signed __int32 int32_t;
+typedef signed __int64 int64_t;
+
+typedef unsigned __int8 uint8_t;
+typedef unsigned __int16 uint16_t;
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int64 uint64_t;
+
+typedef long ssize_t;
+
+#else
 #include <stdint.h>
+#endif
 #include <limits.h>
 
 /* Version of Verse protocol */
